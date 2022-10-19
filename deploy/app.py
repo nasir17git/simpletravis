b19358460d0a4612ac5e3dc4ed2e099b4abe190f
nasir17git <nasir17.dev@gmail.com>
2022-10-19 14:46:51 +0900

    deploy test

--- deploy/app.py
+++ deploy/app.py
@@ -2,11 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! test15</p>"
+    return "<p>Hello, World! test16</p>"
+
 
 @app.route("/health_check")
 def health_check():
     return "OK"
-
