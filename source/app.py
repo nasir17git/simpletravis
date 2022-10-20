44a36ad97d7ae47f3662ec75aefc28294ec0d3a5
nasir17git <nasir17.dev@gmail.com>
2022-10-20 10:57:47 +0900

    test20

--- source/app.py
+++ source/app.py
@@ -1,12 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-qweqwewqeeqwe
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! test19</p>"
-
+    return "<p>Hello, World! test20</p>"
 
 @app.route("/health_check")
 def health_check():
