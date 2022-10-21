cbf3195325f20dda57b3e93b94e37fc79a073cf0
nasir17git <nasir17.dev@gmail.com>
2022-10-21 15:28:38 +0900

    노체크아웃

--- source/app.py
+++ source/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! 221021</p>"
+    return "<p>Hello, World! 221021v1</p>"
 
 @app.route("/health_check")
 def health_check():
