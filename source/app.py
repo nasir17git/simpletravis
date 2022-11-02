1ea66f99e160eaec9c5d36f71139bb0a8c5ef2a7
nasir17git <nasir17.dev@gmail.com>
2022-11-02 10:31:16 +0900

    221102_v1

--- source/app.py
+++ source/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! 221027v1</p>"
+    return "<p>Hello, World! 221102v1</p>"
 
 @app.route("/health_check")
 def health_check():
